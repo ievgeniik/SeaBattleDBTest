da99056c6fdf2932267613cd0d8bedcbea5978e7
ievgeniik <kyyenkoromanyukye@gmail.com>
2016-07-25 07:59:33 +0300

    adding decorator

---
+++ Decorator/DecoratorExampleLibrary/DecoratorExampleLibrary/LibraryItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorExampleLibrary
+{
+    abstract class LibraryItem
+    {
+        private int _numCopies;
+
+        public int NumCopies
+        {
+            get
+            {
+                return _numCopies;
+            }
+            set
+            {
+                _numCopies = value;
+            }
+        }
+
+        public abstract void Display();
+    }
+}
---
+++ Decorator/DecoratorExampleLibrary/DecoratorExampleLibrary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorExampleLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Worley", "Inside ASP.NET", 10);
+            book.Display();
+
+            Video video = new Video("Spielberg", "Jaws", 23, 92);
+            video.Display();
+
+            Console.WriteLine("\nMaking video borrowable:");
+
+            Borrowable borrowvideo = new Borrowable(video);
+            borrowvideo.BorrowItem("Customer #1");
+            borrowvideo.BorrowItem("Customer #2");
+
+            borrowvideo.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
